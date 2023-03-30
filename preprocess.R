aa82e7559079f1b027d7aba8fd81ada50025a025
Dave Slager <slager@users.noreply.github.com>
2023-03-30 18:49:48 +0000

    successful batch preprocess multiple species

--- preprocess.R
+++ preprocess.R
@@ -19,16 +19,23 @@ reg$cluster.functions <- makeClusterFunctionsSlurm(template = 'test_template.tmp
 dir <- 'batch_preprocess'
 dir.create(dir, showWarnings = FALSE)
 batchMap(fun = preprocess_species,
-         species = c('rewbla', 'Hooded Warbler'),
+         species = c('rewbla', 'Hooded Warbler', 'amewoo', 'killde'),
          out_dir = dir,
          gpu_ram = 1)
 
-
-
-rez <- list(walltime = 180, ncpus = 2, memory = 4000, partition = 'cpu-preempt')
+rez <- list(walltime = 180, ncpus = 2, memory = 4000, partition = 'cpu-preempt', max.concurrent.jobs = 4)
 
 submitJobs(resources = rez)
 waitForJobs()
-lapply(1:2, loadResult)
+
+my_result_list <- lapply(1:4, loadResult)
+
+my_species_vector <- unlist(lapply(my_result_list, function(i){i$species$species_code}))
+my_resolution_vector <- unlist(lapply(my_result_list, function(i){i$geom$res[1]/1000}))
+my_ebirdst_year_vector <- unlist(lapply(my_result_list, function(i){i$metadata$ebird_version_year}))
+
+my_species_vector
+my_resolution_vector
+my_ebirdst_year_vector
 
 #getJobTable()
