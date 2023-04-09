54cdad89c4cf30f874c5c0075644b5d7a45fec64
Dave Slager <slager@users.noreply.github.com>
2023-04-09 17:14:10 +0000

    use default ncpus in conf file

--- batchtools.conf.R
+++ batchtools.conf.R
@@ -1,3 +1,6 @@
 cluster.functions <- makeClusterFunctionsSlurm(template = 'slurm.tmpl',
                                                array.jobs = TRUE,
                                                nodename = 'login5')
+default.resources <- list(
+  ncpus = 1
+)
