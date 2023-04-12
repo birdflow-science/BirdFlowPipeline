3352b52bcfe9be1bb231cdd97995bcf38fa1616b
Dave Slager <slager@users.noreply.github.com>
2023-04-12 21:20:04 +0000

    adjust node constraints for faster GPUs

--- batchtools.conf.R
+++ batchtools.conf.R
@@ -10,3 +10,12 @@ default.resources <- list(
   partition.cpu = 'cpu-preempt,cpu',
   constraint.gpu = '2080|2080ti|v100'
 )
+
+# broader: constraint.gpu = '2080|2080ti|rtx8000|v100'
+# but RTX 8000 seems a little slower sometimes
+# benchmarks:
+# ials 2 minutes
+# 1080ti 3:30 to 5:00
+# titanx 5:25
+# v100 1:30
+# rtx8000 2:20 - 3 min
