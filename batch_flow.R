e8acb27012fc0f6e09cf78c482804fe40ef158bd
Dave Slager <slager@users.noreply.github.com>
2023-04-21 22:31:06 +0000

    modifications for grid searches and visualizations

--- batch_flow.R
+++ batch_flow.R
@@ -1,6 +1,6 @@
 #Sys.setenv(DEBUGME = "batchtools")
 
-my_packages <- c('data.table', 'dplyr', 'tidyr', 'BirdFlowR', 'batchtools')
+my_packages <- c('data.table', 'dplyr', 'tidyr', 'BirdFlowR', 'batchtools', 'rgl')
 for (i in my_packages){
   library(i, character.only = TRUE)
 }
@@ -40,9 +40,9 @@ batchMap(fun = birdflow_modelfit,
              myres = pp_info$res
            ),
            grid_search_list = list(
-             dist_weight = 0.005,
-             ent_weight = seq(from = 0, to = 0.006, by = 0.001),
-             dist_pow = seq(from = 0.1, to = 1.0, by = 0.1)
+             dist_weight = seq(from = 0.01, to = 0.51, length.out = 4),
+             ent_weight = seq(from = 0.01, to = 0.11, length.out = 4),
+             dist_pow = seq(from = 0.1, to = 0.7, length.out = 4)
            )
          ),
          reg = makeRegistry(paste0(make_timestamp(), '_mf'), conf.file = 'batchtools.conf.R'))
@@ -74,7 +74,6 @@ waitForJobs()
 # btmapply version of the above
 files <- list.files(path = my_dir, pattern = '^amewoo.*58km_.*\\.hdf5$', full.names = TRUE)
 names(files) <- basename(files) %>% sub('\\.hdf5', '', .)
-files <- files[1:2]
 
 aa <- btmapply(
   do_ll_plain,
@@ -89,7 +88,38 @@ aa <- btmapply(
                    memory = 8),
   n.chunks = 1L
 )
-  
+
+hyperparams_ll_df_row <- function(i){
+  mn <- i$model
+  tibble(
+    obs = sub('.*obs(.*?)_.*', '\\1', mn) %>% as.numeric,
+    ent = sub('.*ent(.*?)_.*', '\\1', mn) %>% as.numeric,
+    dist = sub('.*dist(.*?)_.*', '\\1', mn) %>% as.numeric,
+    pow = sub('.*pow(.*?)\\.hdf5', '\\1', mn) %>% as.numeric,
+    ll = sum(i$ll$log_likelihood, na.rm = TRUE)
+  )
+}
+
+ll_df <- lapply(aa, hyperparams_ll_df_row) %>% bind_rows %>% arrange(-ll)
+ll_df$color <- hcl.colors(15, rev = TRUE)[cut(ll_df$ll, 15)]
+
+# Plot
+plot3d( 
+  x = ll_df$ent, y = ll_df$dist, z = ll_df$pow, 
+  col = ll_df$color, 
+  type = 's', 
+  radius = .02,
+  xlab="ent", ylab="dist", zlab="pow")
+
+# To display in an R Markdown document:
+# rglwidget()
+
+# To save to a file:
+htmlwidgets::saveWidget(rglwidget(width = 520, height = 520), 
+                        file = "3dscatter.html",
+                        libdir = "libs",
+                        selfcontained = TRUE
+)
 
 
 ## from PUFI.R code ##
