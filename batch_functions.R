9ea15b15ca3bb41d5f6b6b45fd67761894453bb6
Dave Slager <slager@users.noreply.github.com>
2023-04-24 23:28:18 +0000

    add null_ll to output df

--- batch_functions.R
+++ batch_functions.R
@@ -70,6 +70,7 @@ hyperparams_ll_df_row <- function(i){
     ent = sub('.*ent(.*?)_.*', '\\1', mn) %>% as.numeric,
     dist = sub('.*dist(.*?)_.*', '\\1', mn) %>% as.numeric,
     pow = sub('.*pow(.*?)\\.hdf5', '\\1', mn) %>% as.numeric,
-    ll = sum(i$ll$log_likelihood, na.rm = TRUE)
+    ll = sum(i$ll$log_likelihood, na.rm = TRUE),
+    nll = sum(i$ll$null_ll, na.rm = TRUE)
   )
 }
