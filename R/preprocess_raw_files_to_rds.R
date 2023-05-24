c8a46a91ed17adae9815327621d4a6877b24a2ff
Dave Slager <slager@users.noreply.github.com>
2023-05-24 20:00:59 +0000

    make functions and move script to R

--- R/preprocess_raw_files_to_rds.R
+++ R/preprocess_raw_files_to_rds.R
@@ -1,15 +1,9 @@
-library(data.table)
-library(dplyr)
-library(batchtools)
-library(magrittr)
-source(file.path('R', 'batch_functions.R'))
-source(file.path('R', 'functions.R'))
-
-# Some file sets need to be collapsed together for processing because same ebirdst taxa span multiple files
-collapse_list <- list(1, c(2,3), 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, c(42, 43), 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, c(55, 56), 57)
-collapse_list <- lapply(collapse_list, function(i) file.path('data', paste0('NABBP_2022_grp_', sprintf('%02d', i), '.csv')))
-  
-if (!dir.exists('rds')) {dir.create('rds')}
+# library(data.table)
+# library(dplyr)
+# library(batchtools)
+# library(magrittr)
+# source(file.path('R', 'batch_functions.R'))
+# source(file.path('R', 'functions.R'))
 
 # Function to take a set of raw files and produce per-species RDS
 process_file_set <- function(collapse_list_item){
@@ -29,6 +23,13 @@ process_file_set <- function(collapse_list_item){
   }
 }
 
+batch_preprocess_raw_files_to_rds <- function(){
+
+# Some file sets need to be collapsed together for processing because same ebirdst taxa span multiple files
+collapse_list <- list(1, c(2,3), 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, c(42, 43), 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, c(55, 56), 57)
+collapse_list <- lapply(collapse_list, function(i) file.path('data', paste0('NABBP_2022_grp_', sprintf('%02d', i), '.csv')))
+
+if (!dir.exists('rds')) {dir.create('rds')}
 
 ## Batch process raw files to RDS
 
@@ -44,3 +45,4 @@ submitJobs(mutate(findNotSubmitted(), chunk = 1L),
            resources = list(walltime = 60,
                             memory = 8))
 waitForJobs()
+}
