9b4cb2641a079a66a76c8f807cc24b7645154fb8
Dave Slager <slager@users.noreply.github.com>
2023-05-24 20:19:22 +0000

    move banding data to work directory

--- R/download_files.R
+++ R/download_files.R
@@ -6,16 +6,17 @@
 # library(rvest)
 # library(tidyr)
 
-download_banding_files <- function(){
+# /work/pi_drsheldon_umass_edu/birdflow_modeling/dslager_umass_edu/banding_data
+download_banding_files <- function(banding_data_dir){
 
 # path management ---------------------------------------------------------
 
-if (!dir.exists('data')) {dir.create('data')}
+if (!dir.exists(banding_data_dir)) {dir.create(banding_data_dir)}
 
 # get datafile info -------------------------------------------------------
 
 #read_html('https://www.sciencebase.gov/catalog/item/632b2d7bd34e71c6d67bc161')
-file_info <- read_html('data/htmlpage.htm') %>% html_nodes('td span.sb-download-link')
+file_info <- read_html(file.path(banding_data_dir, 'htmlpage.htm')) %>% html_nodes('td span.sb-download-link')
 file_df <- data.frame(
   url = html_attr(file_info, 'data-url'),
   name = html_text(file_info))
@@ -25,7 +26,7 @@ file_df$url <- paste0('https://www.sciencebase.gov', file_df$url)
 
 for (i in seq_len(nrow(file_df))){
   # download in reverse order (earlier files are huge)
-  file_path <- file.path('data', file_df$name[i])
+  file_path <- file.path(banding_data_dir, file_df$name[i])
   download.file(file_df$url[i], file_path)
 }
 
