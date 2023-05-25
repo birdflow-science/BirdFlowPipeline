48daf33efba60873d4211b83665c54cfd9e53013
Dave Slager <slager@users.noreply.github.com>
2023-05-25 00:29:30 +0000

    more namespace and dependency stuff

---
+++ R/banding-package.R
@@ -0,0 +1,17 @@
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+#' @importFrom data.table fread
+#' @importFrom grDevices dev.off
+#' @importFrom grDevices hcl.colors
+#' @importFrom grDevices pdf
+#' @importFrom stats cor
+#' @importFrom stats na.omit
+#' @importFrom stats princomp
+#' @importFrom utils capture.output
+#' @importFrom utils download.file
+#' @importFrom utils tail
+#' @importFrom utils write.csv
+## usethis namespace: end
+NULL
