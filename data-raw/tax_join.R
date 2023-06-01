5ffe0dec50388079a373f840e9ebdd9dc4c217e0
Dave Slager <slager@users.noreply.github.com>
2023-06-01 19:32:59 +0000

    remove large internal sysdata

--- data-raw/tax_join.R
+++ data-raw/tax_join.R
@@ -2,4 +2,4 @@
 
 tax_join <- data.table::fread(file.path('data-raw', 'eBird_Taxonomy_v2021.csv')) %>% select(SPECIES_CODE, PRIMARY_COM_NAME)
 
-usethis::use_data(tax_join, internal = TRUE)
+usethis::use_data(tax_join)
