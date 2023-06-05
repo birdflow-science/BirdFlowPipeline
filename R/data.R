d7dbe6e7cde9e99d67b79009052263f6b2b7b88a
Dave Slager <slager@users.noreply.github.com>
2023-06-05 14:45:46 -0700

    file download urls df to data/

---
+++ R/data.R
@@ -0,0 +1,11 @@
+#' URLs for downloading raw USGS banding datafiles version 2022-07-14
+#'
+#' @format ## `banding_raw_file_urls`
+#' A data frame with 60 obs. of 2 variables, scraped from the html source at the below DOI
+#' \describe{
+#'   \item{url}{download URL}
+#'   \item{name}{filename}
+#'
+#' }
+#' @source <https://dx.doi.org/10.5066/P9BSM38F>
+"banding_raw_file_urls"
--- R/download_files.R
+++ R/download_files.R
@@ -3,7 +3,6 @@
 
 # library(data.table)
 # library(dplyr)
-# library(rvest)
 # library(tidyr)
 
 # /work/pi_drsheldon_umass_edu/birdflow_modeling/dslager_umass_edu/banding_data
@@ -13,21 +12,12 @@ download_banding_files <- function(banding_data_dir){
 
 if (!dir.exists(banding_data_dir)) {dir.create(banding_data_dir)}
 
-# get datafile info -------------------------------------------------------
-
-#read_html('https://www.sciencebase.gov/catalog/item/632b2d7bd34e71c6d67bc161')
-file_info <- rvest::read_html(file.path(banding_data_dir, 'htmlpage.htm')) %>% rvest::html_nodes('td span.sb-download-link')
-file_df <- data.frame(
-  url = rvest::html_attr(file_info, 'data-url'),
-  name = rvest::html_text(file_info))
-file_df$url <- paste0('https://www.sciencebase.gov', file_df$url)
-
 ### Download files
 
-for (i in seq_len(nrow(file_df))){
+for (i in seq_len(nrow(banding_raw_file_urls))){
   # download in reverse order (earlier files are huge)
-  file_path <- file.path(banding_data_dir, file_df$name[i])
-  utils::download.file(file_df$url[i], file_path)
+  file_path <- file.path(banding_data_dir, banding_raw_file_urls$name[i])
+  utils::download.file(banding_raw_file_urls$url[i], file_path)
 }
 
 }
