a756a57b7b6b1aa39085278a4dff06a68984d199
Dave Slager <slager@users.noreply.github.com>
2023-06-08 15:56:27 -0700

    last of legacy namespace issues for devtools::check

--- R/data_availability.R
+++ R/data_availability.R
@@ -6,10 +6,12 @@
 
 # Batch data availability info function
 banding_data_availability <- function(file){
-  tax_join <- fread(file.path('data-raw', 'bandng_taxonomy', 'eBird_Taxonomy_v2021.csv')) %>% select(SPECIES_CODE, PRIMARY_COM_NAME)
+  tax_join <- data.table::fread(file.path('data-raw', 'bandng_taxonomy', 'eBird_Taxonomy_v2021.csv')) %>%
+    dplyr::select(.data$SPECIES_CODE, .data$PRIMARY_COM_NAME) %>%
+    dplyr::distinct()
   df <- readRDS(file)
-  species_code <- basename(file) %>% sub('\\.rds$', '', .)
-  species_name <- tax_join[SPECIES_CODE == species_code,]$`PRIMARY_COM_NAME`
+  species_code <- sub('\\.rds$', '', basename(file))
+  species_name <- dplyr::filter(tax_join, .data$SPECIES_CODE == species_code) %>% dplyr::pull(.data$PRIMARY_COM_NAME)
   message(species_name)
   max_track_days_options <- c(Inf, 180)
   min_distance_km_options <- c(0, 0.001, 15)
@@ -19,13 +21,13 @@ banding_data_availability <- function(file){
                              min_distance_km = rep(min_distance_km_options, 2),
                              n_tracks = rep(NA_integer_, 6))
   df <- preprocess_calc_distance_days(df)
-  df <- drop_na(df, distance, days)
-  df <- dplyr::filter(df, days > 0)
+  df <- tidyr::drop_na(df, .data$distance, .data$days)
+  df <- dplyr::filter(df, .data$days > 0)
   # stats for all durations
   for (da in max_track_days_options){
     for (di in min_distance_km_options){
       attrition_df[attrition_df$max_track_days == da & attrition_df$min_distance_km == di, 'n_tracks'] <- 
-        dplyr::filter(df, days <= da & distance >= di) %>% nrow
+        dplyr::filter(df, .data$days <= da & .data$distance >= di) %>% nrow
     }
   }
   # utils::write.csv(attrition_df, file.path('output','attrition.csv'), row.names = FALSE)
@@ -45,15 +47,13 @@ my_suffix <- 'da'
 batchtools::batchMap(banding_data_availability,
          rds_files,
          reg = batchtools::makeRegistry(paste(make_timestamp(), my_suffix, sep = '_'),
-                            conf.file = system.file('batchtools.conf.R', package = 'banding'),
-                            packages = my_packages,
-                            source = file.path('R', 'functions.R'),
+                            conf.file = system.file('batchtools.conf.R', package = 'banding')
          ))
-batchtools::submitJobs(mutate(batchtools::findNotSubmitted(), chunk = 1L),
+batchtools::submitJobs(dplyr::mutate(batchtools::findNotSubmitted(), chunk = 1L),
            resources = list(walltime = 30,
                             memory = 4))
 batchtools::waitForJobs()
-results_df <- lapply(batchtools::findJobs()$job.id, batchtools::loadResult) %>% rbindlist
+results_df <- lapply(batchtools::findJobs()$job.id, batchtools::loadResult) %>% data.table::rbindlist
 utils::write.csv(results_df, 'data_availability.csv', row.names = FALSE)
 
 hindex <- function(x) {
@@ -62,6 +62,6 @@ hindex <- function(x) {
 }
 
 results_df %>%
-  group_by(max_track_days, min_distance_km) %>%
-  summarise(n_species_n_tracks_h_index = hindex(n_tracks), .groups = 'drop')
+  dplyr::group_by(.data$max_track_days, .data$min_distance_km) %>%
+  dplyr::summarise(n_species_n_tracks_h_index = hindex(.data$n_tracks), .groups = 'drop')
 }
