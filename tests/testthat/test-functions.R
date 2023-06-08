20d34393f25e7289a1b69d14463756dd233ec353
Dave Slager <slager@users.noreply.github.com>
2023-06-08 15:33:16 -0700

    tests for rts_stats

--- tests/testthat/test-functions.R
+++ tests/testthat/test-functions.R
@@ -14,3 +14,20 @@ test_that("evaluate_model works", {
   expect_null(eval_obj$bf$metadata$hyperparameters$dist_weight)
   expect_null(eval_obj$bf$metadata$hyperparameters$dist_pow)
 })
+
+test_that("routes stats work", {
+  # test route generation
+  withr::with_seed(42,
+                   {
+                     expect_no_error({
+                       rts <- BirdFlowR::route_migration(BirdFlowModels::rewbla, 100, 'prebreeding')
+                     })
+                   })
+  # test route stats
+  expect_no_error({
+    stats <- rts_stats(rts)
+  })
+  expect_equal(stats,
+               list(straightness = 0.823304236754776, sinuosity = 0.00184305321524582, 
+                    length = 1463.41057052456, displacement = 845.890889687627))
+})
