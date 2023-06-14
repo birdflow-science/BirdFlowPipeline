da0bfdcc27b803e9d06f978df872100c5a42a394
Dave Slager <slager@users.noreply.github.com>
2023-06-14 16:29:35 +0000

    make batch-flow test interactive-only and cleanup tmpfiles

--- tests/testthat/test-batch_flow.R
+++ tests/testthat/test-batch_flow.R
@@ -1,4 +1,7 @@
 test_that("batch_flow works", {
+  # test fails using devtools::check(), for confusing reasons
+  # this line makes this test run with devtools::test() but not devtools::check()
+  skip_if_not(interactive())
   # skip if not linux
   skip_on_os(
     c("windows", "mac", "solaris")
@@ -8,19 +11,24 @@ test_that("batch_flow works", {
     system2('hostname', '-d', stdout = TRUE) == "unity.rc.umass.edu"
   )
   # test batch_flow()
-  test_hdf_dir <- "/work/pi_drsheldon_umass_edu/birdflow_modeling/dslager_umass_edu/batch_hdf/amewoo_700km_TEST"
-  test_output_path <- "/home/dslager_umass_edu/banding_output/amewoo_700km_TEST"
+  test_species <- 'American Woodcock'
+  test_species <- ebirdst::get_species(test_species)
+  test_res <- 700
+  tempdir1 <- system('mktemp -d --tmpdir=$HOME .tmpdir-XXXXXXXX', intern = TRUE)
+  tempdir2 <- system('mktemp -d --tmpdir=$HOME .tmpdir-XXXXXXXX', intern = TRUE)
   on.exit({
-    if (file.exists(test_hdf_dir)) unlink(test_hdf_dir, recursive = TRUE)
-    if (file.exists(test_output_path)) unlink(test_output_path, recursive = TRUE)
+    if (file.exists(tempdir1)) unlink(tempdir1, recursive = TRUE)
+    if (file.exists(tempdir2)) unlink(tempdir2, recursive = TRUE)
   })
+  test_hdf_dir <- tempdir1
+  test_output_path <- tempdir2
   expect_no_error(
     batch_flow(
-      one_species = 'American Woodcock',
+      one_species = test_species,
       params = list(
         my_species = character(0),
         gpu_ram = 10,
-        my_res = 700,
+        my_res = test_res,
         output_nickname = 'TEST',
         grid_search_type = 'new',
         grid_search_list = list(
@@ -29,15 +37,17 @@ test_that("batch_flow works", {
           dist_pow = seq(from = 0.2, to = 0.8, by = 0.15),
           dist_weight = NA_real_,
           ent_weight = NA_real_),
-        batch_hdf_path = the$batch_hdf_path,
-        banding_output_path = the$banding_output_path
+        batch_hdf_path = test_hdf_dir,
+        banding_output_path = test_output_path
       )
     )
   )
   expect_true(file.exists(test_hdf_dir))
   expect_true(file.exists(test_output_path))
+  output_fullname <- paste(test_species, paste0(test_res, 'km'), 'TEST', sep = '_')
+  test_output_path <- file.path(test_output_path, output_fullname)
   test_output_files <- list.files(test_output_path)
-  test_hdf_files <- list.files(test_hdf_dir, full.names = TRUE)
+  test_hdf_files <- list.files(file.path(test_hdf_dir, output_fullname), full.names = TRUE)
   expect_no_error(ll_df <- readRDS(file.path(test_output_path, 'll_df.rds')))
   # two file sizes -- unfitted hdf5 and fitted hdf5s
   expect_true(length(unique(file.size(test_hdf_files))) == 2)
