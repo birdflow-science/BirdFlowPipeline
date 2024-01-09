0ff2a8517c356c216ca6ca2c56b8ac523a117151
Dave Slager <slager@users.noreply.github.com>
2024-01-09 18:43:54 +0000

    add package required in report Rmd file

--- R/misc.R
+++ R/misc.R
@@ -3,4 +3,7 @@ ignore_unused_imports <- function() {
   # https://r-pkgs.org/dependencies-in-practice.html#how-to-not-use-a-package-in-imports
   # need to use unexported autoplot.princomp method from ggfortify in model_evaluation_biplot()
   ggfortify::ggbiplot
+  # need kableExtra in the Rmd file in inst for report generation,
+  # which is not checked by devtools::check()
+  kableExtra::kable
 }
