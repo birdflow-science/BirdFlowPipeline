b495d8d90dbfb47c3a218403f841a946c13a764b
Ethan Plunkett <ethan@plunkettworks.com>
2024-02-09 16:39:18 -0500

    add gpu_ram()

--- NEWS.md
+++ NEWS.md
@@ -3,8 +3,9 @@
 ## BirdFlowPipeline 0.0.9001
 Feb 9, 2024
 
-* Added NEWS.md
-* Added schema outlining major package functions, how they call each other,
+* Add NEWS.md
+* Add schema outlining major package functions, how they call each other,
 and what files they write.
 
+* Add `gpu_ram()` to calculate the GB of GPU ram needed to fit a model
 
---
+++ R/gpu_ram.R
@@ -0,0 +1,25 @@
+#' Calculate the GPU RAM needed for a model
+#'
+#' @param bf A BirdFlow model object 
+#' @param max_param_per_gb The number of parameters that can be fit with each
+#' GB of GPU ram. If `NULL`, the default the value from
+#' `BirdFlowOptions()` is used. The only reason to set it here is if that value
+#' is empirically wrong. 
+#'
+#' @return The GB of GPU RAM that was or will be required to fit `bf`
+#' @export
+#'
+#' @examples
+#' bf <- BirdFlowModels::amewoo
+#' gpu_ram(bf)
+gpu_ram <- function(bf, max_param_per_gpu_gb = NULL){
+  if(is.null(max_param_per_gb)){
+    max_param_per_gpu_gb <- BirdFlowR::birdflow_options()$max_param_per_gpu_gb
+  }
+  stopifnot(is.numeric(max_param_per_gpu_gb), 
+            length(max_param_per_gpu_gb) == 1, 
+            !is.na(max_param_per_gpu_gb))
+  
+  
+  return(BirdFlowR::n_parameters(bf) / max_param_per_gpu_gb)
+}
